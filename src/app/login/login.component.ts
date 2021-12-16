d5d03955bb7047cc22b63e5e88af8af245c44485
sanjivpnf <sanjivpnf@gmail.com>
2021-12-16 18:20:23 +0530

    Test push Dev error

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,7 +4,7 @@ import { NavbarService } from '../services/navbar.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'
 })
 export class LoginComponent implements OnInit {
 
